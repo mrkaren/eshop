f191613a59762548850cb1ed3f31f32e88165646
Karen Gasparyan <mrkaren92@gmail.com>
2024-02-18 12:48:25 +0400

    i18n, logs

--- src/main/java/am/itspace/eshop/service/impl/UserServiceImpl.java
+++ src/main/java/am/itspace/eshop/service/impl/UserServiceImpl.java
@@ -4,12 +4,14 @@ import am.itspace.eshop.entity.User;
 import am.itspace.eshop.repository.UserRepository;
 import am.itspace.eshop.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -21,7 +23,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAll() {
-        return userRepository.findAll();
+        List<User> all = userRepository.findAll();
+        log.info("find all users, users list size {}", all.size());
+        return all;
     }
 
     @Override
